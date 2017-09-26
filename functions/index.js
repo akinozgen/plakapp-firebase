658cd079b954a05c8a843de84bd6eb027c032e93
Akın Özgen <akinozgen17@outlook.com>
2017-09-27 02:35:20 +0300

    send notifications to followers of plates done

--- functions/index.js
+++ functions/index.js
@@ -30,3 +30,48 @@ exports.updateRatingAverageOnWrite = functions
         });
 
     });
+
+
+exports.sendNotificationToFollowers = functions
+    .database
+    .ref('/comments/{commentId}')
+    .onWrite(event => {
+
+        const { commentId } = event.params;
+        event = event.data.toJSON();
+        const plateId = event.plate;
+
+        admin
+            .database()
+            .ref(`/plates/${plateId}`)
+            .on('value', snaps => {
+                snaps = snaps.toJSON();
+
+                if (snaps.follows == null) return;
+
+                console.log('PLATE DATA', snaps);
+
+                Object.keys(snaps.follows).map(snap => {
+                    uid = snaps.follows[snap];
+
+                    console.log('USER ID', uid);
+                    console.log('SNAP', snap);
+
+                    admin
+                        .database()
+                        .ref(`/users/${uid}/token`)
+                        .on('value', userSnap => {
+                            userSnap = userSnap.toJSON();
+
+                            return admin.messaging().sendToDevice(userSnap, {
+                                notification: {
+                                    title: 'Yeni Yorum',
+                                    body: `${snaps.code} plakasına yeni yorum gönderildi. "${event.comment}"`,
+                                }
+                            })
+                        });
+                });
+            });
+
+
+    });
