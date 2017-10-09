720d0d7190d6937e18df5c23b1331aa65395c551
Akın Özgen <akinozgen17@outlook.com>
2017-10-09 03:04:49 +0300

    new functions

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const request = require('request');
 
 admin.initializeApp(functions.config().firebase);
 
@@ -7,43 +8,76 @@ exports.sendNotificationToFollowers = functions
     .database
     .ref('/comments/{commentId}')
     .onCreate(event => {
-
-        if (event.eventType != 'providers/google.firebase.database/eventTypes/ref.create') return;
-
         const { commentId } = event.params;
         event = event.data.toJSON();
         const plateId = event.plate;
         const realOwner = event.realOwner;
 
-        admin
-            .database()
-            .ref(`/plates/${plateId}`)
-            .on('value', snaps => {
-                snaps = snaps.toJSON();
+        admin.database().ref(`/plates/${plateId}/code`).once('value', plateSnap => {
+            plateSnap = plateSnap.toJSON();
 
-                if (snaps.follows == null) return;
+            if (plateSnap != null)
+                admin.database().ref(`/users/${realOwner}/token`).once('value', tokenSnap => {
+                    tokenSnap = tokenSnap.toJSON();
 
-
-                Object.keys(snaps.follows).map(snap => {
-                    uid = snaps.follows[snap];
-
-                    if (uid === realOwner) return;
+                    if (tokenSnap == null) return;
 
                     admin
-                        .database()
-                        .ref(`/users/${uid}/token`)
-                        .on('value', userSnap => {
-                            userSnap = userSnap.toJSON();
-
-                            return admin.messaging().sendToDevice(userSnap, {
-                                notification: {
-                                    title: 'Yeni Yorum',
-                                    body: `${snaps.code} plakasına yeni yorum gönderildi. "${event.comment}"`,
-                                }
-                            })
+                        .messaging()
+                        .unsubscribeFromTopic(tokenSnap, plateId)
+                        .then(onresolve => {
+                            if (onresolve.successCount > 0) {
+                                admin.messaging().sendToTopic(plateId, {
+                                    notification: {
+                                        title: 'Yeni Yorum',
+                                        body: `${plateSnap} plakasına yeni yorum gönderildi. "${event.comment}"`,
+                                    }
+                                }).then(onfullfilled => {
+                                    admin.messaging().subscribeToTopic(tokenSnap, plateId);
+                                });
+                            }
                         });
                 });
-            });
+        });
+    });
 
+exports.createUserOnNewAuth = functions
+    .auth
+    .user()
+    .onCreate(event => {
+        const { data } = event;
 
+        let payload = {
+            id: data.uid,
+            username: `Yeni Kullanıcı ${Date.now()}`
+        };
+
+        admin
+            .database()
+            .ref(`/users/${data.uid}`).update(payload);
+    });
+
+exports.subscribeToTopic = functions
+    .database
+    .ref('/users/{userId}/follows/')
+    .onWrite(event => {
+        const plate = event.data.toJSON();
+        const { userId } = event.params;
+
+        console.log('PLATE', plate);
+
+        if (plate == null) return;
+
+        console.log('PLATE', plate);
+
+        admin.database().ref(`/users/${userId}/token`).once('value', tokenSnap => {
+            tokenSnap = tokenSnap.toJSON();
+
+            if (tokenSnap != null)
+                admin
+                    .messaging()
+                    .subscribeToTopic(tokenSnap, plate[Object.keys(plate)[0]])
+                    .then(onfullfilled => console.log('RESPONSE', onfullfilled))
+                    .catch(onrejected => console.log('ERROR', onrejected));
+        });
     });
